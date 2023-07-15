8d15c2896ea485d9455d076e9d2586ea087ccdd3
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2023-07-16 01:44:15 +0900

    Feat: 머스테치를 이용한 화면 구성

--- src/main/java/com/example/project/service/posts/PostsService.java
+++ src/main/java/com/example/project/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.example.project.service.posts;
 
 import com.example.project.domain.posts.Posts;
 import com.example.project.domain.posts.PostsRepository;
+import com.example.project.web.dto.PostsListResponseDto;
 import com.example.project.web.dto.PostsResponseDto;
 import com.example.project.web.dto.PostsSaveRequestDto;
 import com.example.project.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -38,5 +42,22 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    // 게시글 전체 목록 조회
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지 but 조회 기능만 남겨둠
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    // 게시글 삭제
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+
+        postsRepository.delete(posts);
     }
 }
